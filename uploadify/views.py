118cd01a3443b5a6948fd273d2d88e505f3a691a
Mohit Ranka <mohitranka@gmail.com>
2011-02-01 14:38:37 +0530

    Fixes issue #1

--- uploadify/views.py
+++ uploadify/views.py
@@ -1,12 +1,12 @@
 from django.http import HttpResponse
 import django.dispatch
 
-upload_recieved = django.dispatch.Signal(providing_args=['data'])
+upload_received = django.dispatch.Signal(providing_args=['data'])
 
 def upload(request, *args, **kwargs):
     if request.method == 'POST':
         if request.FILES:
-            upload_recieved.send(sender='uploadify', data=request.FILES['Filedata'])
+            upload_received.send(sender='uploadify', data=request.FILES['Filedata'])
     return HttpResponse('True')
 
 
