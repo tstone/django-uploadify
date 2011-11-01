01e05b741dabab0bf70004f6565d522b97700eef
Nolan Brubaker <nolan@nbrubaker.com>
2011-11-01 14:52:23 -0400

    Fix missing MEDIA_URL and STATIC_ROOT variables.

--- uploadify/settings.py
+++ uploadify/settings.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 
 # Uploadify root folder path, relative to MEDIA_ROOT
-UPLOADIFY_PATH = settings.UPLOADIFY_PATH = getattr(settings, 'UPLOADIFY_PATH', '%s%s' % (MEDIA_URL, 'js/uploadify/'))
+UPLOADIFY_PATH = settings.UPLOADIFY_PATH = getattr(settings, 'UPLOADIFY_PATH', '%s%s' % (settings.MEDIA_URL, 'js/uploadify/'))
 
 # Upload path that files are sent to
-UPLOADIFY_UPLOAD_PATH = settings.UPLOADIFY_PATH = getattr(settings, 'UPLOADIFY_UPLOAD_PATH', '%s%s' % (STATIC_ROOT, 'uploads/'))
+UPLOADIFY_UPLOAD_PATH = settings.UPLOADIFY_PATH = getattr(settings, 'UPLOADIFY_UPLOAD_PATH', '%s%s' % (settings.STATIC_ROOT, 'uploads/'))
